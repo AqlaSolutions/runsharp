6669538528dd726dbbdf05172d4d034f4d64f600
Stefan Simek <simek@triaxis.sk>
2007-11-24 22:07:02 +0100

    Version changed to 0.1.1

--- RunSharp/Properties/AssemblyInfo.cs
+++ RunSharp/Properties/AssemblyInfo.cs
@@ -55,8 +55,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
 
 [assembly: CLSCompliant(true)]
 
