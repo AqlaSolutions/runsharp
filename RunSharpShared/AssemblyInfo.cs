4ba4f51695c046093e56a4ed663376c309d79e38
Vlad Taranov <wvlad.dev@gmail.com>
2016-05-30 21:51:31 +0300

    snk fixed

--- RunSharpShared/AssemblyInfo.cs
+++ RunSharpShared/AssemblyInfo.cs
@@ -57,8 +57,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.18")]
-[assembly: AssemblyFileVersion("1.0.0.18")]
+[assembly: AssemblyVersion("1.0.0.19")]
+[assembly: AssemblyFileVersion("1.0.0.19")]
 
 //[assembly: CLSCompliant(true)]
 #if !PHONE8 && !SILVERLIGHT
