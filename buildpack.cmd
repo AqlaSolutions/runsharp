3785fe704306396121a1c9c9c99c0c11ee671831
Vladyslav Taranov <wvlad.dev@gmail.com>
2021-01-01 18:48:52 +0200

    release

--- buildpack.cmd
+++ buildpack.cmd
@@ -1,2 +1,7 @@
+del *.nupkg
+del RunSharp\bin\Release\*.nupkg
 @"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild" all.build /p:WarningLevel=0
+@"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild" /t:pack /p:Configuration=Release RunSharp\RunSharp.csproj
+copy /y RunSharp\bin\Release\*.nupkg .
+nuget pack RunSharpIKVM\package.nuspec -Prop Configuration=Release
 pause
